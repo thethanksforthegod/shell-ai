4ed5570051bf4d3ba048b37cf162bed86b520248
thethanksforthegod <41610969+thethanksforthegod@users.noreply.github.com>
2025-06-29 16:55:44 +0300

    Update step.sh

--- step.sh
+++ step.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-cd /workspace/
-# Clone the repository
-git clone https://github.com/stepfun-ai/Step1X-Edit.git
-
-# Change to the project directory
-cd Step1X-Edit
-
-# Install Python requirements
+cd /${WORKSPACE}/
+git clone https://github.com/PKU-YuanGroup/UniWorld-V1
+cd UniWorld-V1
 pip install -r requirements.txt
-
-# Install flash_attn without build isolation
 pip install flash_attn --no-build-isolation
-
-pip install gsutil
-
-# Copy files from Google Cloud Storage
-gsutil -m cp -r gs://step-edit/ /workspace
+pip install gdown
+gdown https://drive.google.com/uc?id=1FeGx_DMX8OabwKppAxl0XLM4I03iAZ13
+python scripts/make_univa_qwen2p5vl_weight.py \
+    --origin_flux_ckpt_path $FLUX_PATH \
+    --origin_qwenvl_ckpt_path $QWENVL_PATH \
+    --save_path ${SAVE_PATH}
