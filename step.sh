7440439ad2d9db1aa2fee8ad43532d4833fcb534
thethanksforthegod <41610969+thethanksforthegod@users.noreply.github.com>
2025-06-30 06:55:08 +0300

    Update step.sh

--- step.sh
+++ step.sh
@@ -4,4 +4,5 @@ cd /${WORKSPACE}/
 git clone https://github.com/PKU-YuanGroup/UniWorld-V1
 cd UniWorld-V1
 pip install -r requirements.txt
-pip install flash_attn --no-build-isolation
+wget https://github.com/Dao-AILab/flash-attention/releases/download/v2.7.1.post4/flash_attn-2.7.1.post4+cu12torch2.5cxx11abiTRUE-cp310-cp310-linux_x86_64.whl
+pip install flash_attn-2.7.1.post4+cu12torch2.5cxx11abiTRUE-cp310-cp310-linux_x86_64.whl
