480b2d31ff3a258bc1b6eff55ff0be9e0e4f0ba8
thethanksforthegod <41610969+thethanksforthegod@users.noreply.github.com>
2025-07-12 15:27:10 +0300

    Update bagel.sh

--- bagel.sh
+++ bagel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
 cd /${WORKSPACE}/
 git clone https://github.com/bytedance-seed/BAGEL.git
 cd /${WORKSPACE}/BAGEL
